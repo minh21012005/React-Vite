4f3b5ef33e8b2e2bb0a18901e10fa621145a7655
minh21012005 <baminhss@gmail.com>
2025-06-24 10:05:49 +0700

    30

--- src/components/learn/MyComponent.jsx
+++ src/components/learn/MyComponent.jsx
@@ -1,6 +1,11 @@
+import './style.css'
+
 const MyComponent = () => {
     return (
-        <div>Hello react</div>
+        <>
+            <div>Hello react</div>
+            <div className='child' style={{ borderRadius: '10px' }}>Hello vite</div>
+        </>
     );
 }
 
