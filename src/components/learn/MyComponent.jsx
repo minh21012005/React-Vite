5f8cb387d4b978918b644f42165a82e71aca2f1c
minh21012005 <baminhss@gmail.com>
2025-06-24 15:07:11 +0700

    31

--- src/components/learn/MyComponent.jsx
+++ src/components/learn/MyComponent.jsx
@@ -1,9 +1,14 @@
 import './style.css'
 
 const MyComponent = () => {
+    const hoidanit = {
+        name: 'minh',
+        age: '20'
+    }
     return (
         <>
-            <div>Hello react</div>
+            <div>Hello {JSON.stringify(hoidanit)}</div>
+            <div>{console.log('hello update')}</div>
             <div className='child' style={{ borderRadius: '10px' }}>Hello vite</div>
         </>
     );
