3007314259a7543943c026e12ba96738895df262
minh21012005 <baminhss@gmail.com>
2025-06-26 20:01:10 +0700

    69

--- src/components/user/user.form.jsx
+++ src/components/user/user.form.jsx
@@ -1,5 +1,6 @@
 import { Button, Input } from "antd";
 import { useState } from "react";
+import axios from "axios";
 
 const UserForm = () => {
 
@@ -9,7 +10,14 @@ const UserForm = () => {
     const [phone, setPhone] = useState("");
 
     const handleClickBtn = () => {
-
+        const URL_BACKEND = "http://localhost:8080/api/v1/user";
+        const data = {
+            fullName: fullName,
+            email: email,
+            password: password,
+            phone: phone
+        }
+        axios.post(URL_BACKEND, data);
     }
 
     return (
