9fece53297e4bb0253e68e8b5c1a411f9f597648
minh21012005 <baminhss@gmail.com>
2025-06-29 20:39:48 +0700

    82

--- src/components/user/view.user.detail.jsx
+++ src/components/user/view.user.detail.jsx
@@ -1,4 +1,4 @@
-import { Drawer } from "antd";
+import { Button, Drawer } from "antd";
 
 const ViewUserDetail = (props) => {
 
@@ -11,17 +11,38 @@ const ViewUserDetail = (props) => {
 
     return (
         <Drawer
+            width={"30vw"}
             title="User Detail"
             closable={{ 'aria-label': 'Close Button' }}
             onClose={onClose}
             open={isModalDetailOpen}
         >
-            <div style={{ display: "flex", gap: "20px", flexDirection: "column" }}>
-                <p>ID: {dataUserDetail ? dataUserDetail._id : ""}</p>
-                <p>Full Name: {dataUserDetail ? dataUserDetail.fullName : ""}</p>
-                <p>Email: {dataUserDetail ? dataUserDetail.email : ""}</p>
-                <p>Phone: {dataUserDetail ? dataUserDetail.phone : ""}</p>
+            {dataUserDetail ? <div style={{ display: "flex", gap: "15px", flexDirection: "column" }}>
+                <p>ID:{dataUserDetail._id}</p>
+                <p>Full Name: {dataUserDetail.fullName}</p>
+                <p>Email: {dataUserDetail.email}</p>
+                <p>Phone: {dataUserDetail.phone}</p>
+                <p>Avatar:</p>
+                <div>
+                    <img height={150} width={200} src={`${import.meta.env.VITE_BACKEND_URL}/images/avatar/${dataUserDetail.avatar}`} alt="" />
+                </div>
+                <div>
+                    <label htmlFor="btnUpload" style={{
+                        display: "block",
+                        width: "fit-content",
+                        marginTop: "15px",
+                        padding: "5px 10px",
+                        background: "orange",
+                        borderRadius: "5px",
+                        cursor: "pointer"
+                    }}>Upload Avatar</label>
+                    <input type="file" hidden id="btnUpload" />
+                </div>
+                {/* <div><Button type="primary">Upload Avatar</Button></div> */}
             </div>
+                :
+                <div></div>
+            }
         </Drawer>
     )
 }
