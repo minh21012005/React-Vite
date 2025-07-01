b31933c07152946d42a61bd93fb62cf5da82809f
minh21012005 <baminhss@gmail.com>
2025-07-01 13:01:54 +0700

    90

--- src/pages/register.jsx
+++ src/pages/register.jsx
@@ -1,10 +1,25 @@
-import { Button, Form, Input } from "antd"
+import { Button, Form, Input, notification } from "antd"
+import { registerUserApi } from "../services/api.service";
+import { useNavigate } from "react-router-dom";
 
 const RegisterPage = () => {
     const [form] = Form.useForm();
+    const navigate = useNavigate();
 
-    const onFinish = (value) => {
-        console.log(value)
+    const onFinish = async (value) => {
+        const res = await registerUserApi(value.fullName, value.email, value.password, value.phone);
+        if (res.data) {
+            notification.success({
+                message: "Register user",
+                description: "Đăng kí user thành công!"
+            })
+            navigate("/login");
+        } else {
+            notification.error({
+                message: "Register user error",
+                description: JSON.stringify(res.message)
+            })
+        }
     }
 
     return (
@@ -40,7 +55,13 @@ const RegisterPage = () => {
                     <Form.Item
                         label="Phone number"
                         name="phone"
-                        rules={[{ required: true, message: 'Please input your phone number!' }]}
+                        rules={[
+                            {
+                                required: true,
+                                pattern: new RegExp(/\d+/g),
+                                message: "Wrong format!"
+                            }
+                        ]}
                     >
                         <Input />
                     </Form.Item>
@@ -48,6 +69,13 @@ const RegisterPage = () => {
                         <Button type="primary" htmlType="submit">
                             Register
                         </Button>
+                        {/* <Button type="primary" onClick={() => {
+                            form.setFieldsValue({
+                                email: "test@gmail.com"
+                            })
+                        }}>
+                            Test
+                        </Button> */}
                     </Form.Item>
                 </div>
             </Form>
